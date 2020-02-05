8941cee83d92de745460221aa7ed15b78ed1f32a
Kristian Arambasic <kristian.arambasic@expedeon.com>
2020-02-05 16:05:58 +0000

    first commit

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kristian.arambasic\desktop\django_practice\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
